e02da2a7029da96ac45e6ca50b294a264184dac5
Maou-Shimazu <eldaddanladi@gmail.com>
2022-05-21 19:39:11 -0700

    emoji cog listener

--- extentions/qt.py
+++ extentions/qt.py
@@ -3,16 +3,23 @@ from discord.ext import commands
 from bot import Bot
 
 MAOU_ID = 859581812246446081
-R_ID = "977742294432231454"
+R_ID = "<:oke:879424724332073020>"
 
 class Qt(commands.Cog):
-    __slots_ = {"bot", }
+    __slots_ = {"bot"}
 
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
-        await payload.message.add_reaction(R_ID)
-async def setup(bot: Bot):
+    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent) -> None:
+        message = await self.bot.guild.get_channel(payload.channel_id).fetch_message(payload.message_id)
+        await message.add_reaction(R_ID)
+
+    # @commands.Cog.listener()
+    # async def on_message(self, message: discord.Message):
+    #     await message.add_reaction(R_ID)
+
+
+def setup(bot: Bot):
     bot.add_cog(Qt(bot))
